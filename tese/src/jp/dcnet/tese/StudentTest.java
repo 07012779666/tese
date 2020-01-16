c932bf2b17728c6cc96b69ec14a4e55ce9460819
07012779666 <56950993+07012779666@users.noreply.github.com>
2020-01-16 17:58:11 +0900

    新規作成００
    1.23

---
+++ tese/src/jp/dcnet/tese/StudentTest.java
@@ -0,0 +1,52 @@
+package jp.dcnet.tese;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import jp.dcnet.java.Student;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentTest {
+	Student student;
+
+    @Before
+    public void setup() {
+        student = new Student("01", 50, 50, 50, 50, 50);
+    }
+
+    @Test
+    public void test1() {
+        assertThat(student.isPass(),is(true));
+    }
+
+    @Test
+    public void test2() {
+        student.setSubject1(49);
+        assertThat(student.isPass(), is(false));
+    }
+
+    @Test
+    public void test3() {
+        student.setSubject2(49);
+        assertThat(student.isPass(), is(false));
+    }
+
+    @Test
+    public void test4() {
+        student.setSubject3(49);
+        assertThat(student.isPass(), is(false));
+    }
+
+    @Test
+    public void test5() {
+        student.setSubject4(49);
+        assertThat(student.isPass(), is(false));
+    }
+
+    @Test
+    public void test6() {
+        student.setSubject5(49);
+        assertThat(student.isPass(), is(false));
+    }
+}
